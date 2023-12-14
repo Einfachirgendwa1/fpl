ef195e9a99aa46ce4d27d9de9ce6ea0b96b6c954
Einfachirgendwa1 <faris@karge.berlin>
2023-12-14 21:07:40 +0100

    Variable assingment (set) is now possible

--- sekunge.py
+++ sekunge.py
@@ -1,9 +1,15 @@
 import os
 import sys
 
+from string import ascii_lowercase as abc
+
+
+variables = {letter: None for letter in abc}
+
 if len(sys.argv) == 1:
-    file = input("No arguments. Please specify filename: ")
-    sys.argv.append(file)
+    # file = input("No arguments. Please specify filename: ")
+    # sys.argv.append(file)
+    sys.argv.append("code.txt")
 
 file = sys.argv[1]
 
@@ -21,3 +27,63 @@ except Exception as e:
 if file_contents is None:
     print("File does not contain anything.")
 
+index = 0
+character = None
+
+def set_character():
+    global index, character
+    character = file_contents[index]
+
+def next_character():
+    global index, character
+    index += 1
+    set_character()
+    return character
+
+while index < len(file_contents):
+    set_character()
+
+    if character.isspace():
+        try:
+            next_character()
+        except IndexError:
+            break
+        else:
+            continue
+
+    if not character.isalpha():
+        print(f"This is not a valid character: {character}")
+        exit(1)
+    
+    if character == "s":
+        # set[variable][value]
+        next_character()
+        if character not in variables:
+            print(f"{character} at {index} is not a variable.")
+            exit(1)
+        
+        variable_name = character
+        try:
+            next_character()
+        except IndexError:
+            variables[variable_name] = None
+            break
+        
+        value = ""
+
+        while index+1 < len(file_contents) and not character.isspace():
+            value += character
+            next_character()
+        
+        if index+1 == len(file_contents):
+            value += character
+
+        
+        if value.isdigit():
+            value = int(value)
+        
+        variables[variable_name] = value
+    
+    index += 1
+
+print(variables)
